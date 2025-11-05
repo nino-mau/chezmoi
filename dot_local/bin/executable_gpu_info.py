791153d08084d074bb847f2546173a2d18b73cc0
nino <nino.mauuary@gmail.com>
2025-11-05 09:40:26 +0100

    chore: auto commit on (date '+%Y-%m-%d %H:%M:%S') from nino-20n3s37900

--- dot_local/bin/executable_gpu_info.py
+++ dot_local/bin/executable_gpu_info.py
@@ -6,5 +6,11 @@ import psutil
 cpu_percent = psutil.cpu_percent(interval=1)
 temps = psutil.sensors_temperatures()
 cpu_temp = temps["thinkpad"][0].current or 0
+mem = psutil.virtual_memory()
+gpu_name = "Radeon RX 9070 XT"
 
-print(f" AMD Ryzen 7 󱓞 {round(cpu_percent):2}%  {round(cpu_temp):2}°C ")
+if "thinkpad" in temps:
+    cpu_temp = temps["thinkpad"][0].current or 0
+    gpu_name = "NVIDIA GeForce MX250"
+
+print(f"󰢮 {gpu_name}   {mem.percent}%  󱓞 {round(cpu_percent):2}%   {round(cpu_temp):2}°C")
