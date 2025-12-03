9c312cd9ccfc743578943f3e26e810f9d1c33d03
nino.mauuary <nino.mauuary@gmail.com>
2025-12-03 23:01:36 +0100

    chore: auto commit on 2025-12-03 23:01:36 from nino-b450mh

--- dot_config/private_fish/conf.d/navigation.fish
+++ dot_config/private_fish/conf.d/navigation.fish
@@ -1,26 +1,21 @@
-# Open ~/.local/share folder
 abbr -a cdshare "cd ~/.local/share"
 
-# Open .config folder
 abbr -a cdconf "cd ~/.config"
 
-# Open dev folder
 abbr -a cddev "cd ~/Documents/dev"
 
-# Open handigital folder
 abbr -a cdhandi "cd ~/Documents/dev/handigital/"
 
-# Go to projects folder
 abbr -a cdproj "cd ~/Documents/dev/projects"
 
-# Go to download folder
 abbr -a cddwl "cd ~/Downloads"
 
-# Go to documents
 abbr -a cddoc "cd ~/Documents"
 
-# Go to tmp folder
 abbr -a cdtmp "cd ~/tmp"
 
-# Go to navilab project 
 abbr -a cdnl "cd ~/Documents/dev/projects/navilab"
+
+abbr -a cdnvim "cd ~/.config/nvim"
+
+abbr -a cdexb "cd ~/Documents/dev/exb"
--- dot_config/private_fish/conf.d/shortcuts.fish
+++ dot_config/private_fish/conf.d/shortcuts.fish
@@ -1,14 +1,27 @@
-# Open ghostty terminal config
-abbr -a cghost "nvim ~/.config/ghostty/config"
-
-# Open gameverse project in lazyvim
-abbr -a lvgv "nvim ~/Documents/dev/projects/gameverse"
-
-# Open navilab project in lazyvim
-abbr -a lvnl "nvim ~/Documents/dev/projects/navilab"
-
 # Reload ghostty terminal config
 abbr -a rghost "source ~/.config/ghostty/config"
 
 # Reload fish config file
 abbr -a rconfig "source ~/.config/fish/config.fish"
+
+#
+# NVIM
+#
+
+abbr -a nv nvim
+
+#
+# TMUX
+#
+
+# Kill all session but current
+abbr -a tmka "tmux kill-session -a"
+
+# Load a session with tmuxp 
+abbr -a tml "tmuxp load"
+
+#
+# SSH
+#
+
+alias sshpi="TERM=xterm-256color ssh nino@nino-pi" # Connect to my pi
