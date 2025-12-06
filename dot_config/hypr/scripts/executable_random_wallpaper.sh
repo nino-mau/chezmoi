0fe36311d1a55d11099d372e4c5509ac407e6803
nino.mauuary <nino.mauuary@gmail.com>
2025-12-07 00:58:24 +0100

    chore: auto commit on 2025-12-07 00:58:24 from nino-b450mh

--- dot_config/hypr/scripts/executable_random_wallpaper.sh
+++ dot_config/hypr/scripts/executable_random_wallpaper.sh
@@ -3,7 +3,7 @@
 WALLDIR="$HOME/.local/share/wallpapers/"
 
 # pick a random image
-PIC=$(find "$WALLDIR" -type f | shuf -n 1)
+PIC=$(find "$WALLDIR" -maxdepth 1 -type f | shuf -n 1)
 
 # set wallpaper
-swww img "$PIC"
+swww img "$PIC" --transition-fps 60 --transition-step 255 --transition-type random
