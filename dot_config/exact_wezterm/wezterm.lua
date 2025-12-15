6a6d8db1d9ec3a41453f9ec7cb51628942a12ecc
NMA <nino.private@proton.me>
2025-12-16 00:24:52 +0100

    update wezterm config

--- dot_config/exact_wezterm/wezterm.lua
+++ dot_config/exact_wezterm/wezterm.lua
@@ -2,6 +2,10 @@ local wez = require("wezterm")
 
 local config = {}
 
+-- === GENERAL ===
+
+config.window_close_confirmation = "NeverPrompt"
+
 -- === FONT ===
 
 config.font = wez.font("SpaceMono Nerd Font")
@@ -12,16 +16,34 @@ config.font = wez.font("SpaceMono Nerd Font")
 
 config.font_size = 15
 
-config.use_fancy_tab_bar = false
-config.window_decorations = "RESIZE"
+config.line_height = 1
 
--- config.line_height = 1.09
+-- === THEME ===
+
+config.color_scheme = "Catppuccin Macchiato (Gogh)"
 
 -- === WINDOW ===
 
 -- Window size
-config.initial_cols = 120
-config.initial_rows = 29
+config.initial_cols = 220
+config.initial_rows = 129
+
+-- Remove title bar
+config.window_decorations = "RESIZE"
+-- Remove title bar but keep control buttons
+-- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+-- Add border to window frame
+config.window_frame = {
+	border_left_width = "1px",
+	border_right_width = "1px",
+	border_bottom_height = "1px",
+	border_top_height = "1px",
+	border_left_color = "494c56",
+	border_right_color = "494c56",
+	border_bottom_color = "494c56",
+	border_top_color = "494c56",
+}
 
 -- Window padding
 config.window_padding = {
@@ -35,11 +57,23 @@ config.window_padding = {
 config.window_background_opacity = 0.5
 config.macos_window_background_blur = 50
 
--- === COLOR SHCEME ===
+-- === INPUTS ===
 
-config.color_scheme = "catppuccin-mocha"
+-- On macos make it so the left opt act as altgr
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = false
+
+-- === KEYBINDINGS ===
+config.keys = {
+	{
+		key = "C",
+		mods = "CTRL|SHIFT",
+		action = wez.action.CopyTo("Clipboard"),
+	},
+}
 
 -- === PLUGINS ===
+
 local bar = wez.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config, {
 	position = "bottom",
