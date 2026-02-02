79ebbe549adccac38be1aed23956e3de50de19ad
nino.mauuary <nino.mauuary@gmail.com>
2026-02-03 00:37:35 +0100

    add fish shortcuts

--- dot_config/exact_private_fish/exact_conf.d/shortcuts.fish
+++ dot_config/exact_private_fish/exact_conf.d/shortcuts.fish
@@ -1,32 +1,22 @@
 # Install with paru
 alias i="paru -S"
 
+alias oc="opencode"
+
+alias lg="lazygit"
+
 # Reload ghostty terminal config
 abbr -a rghost "source ~/.config/ghostty/config"
 
 # Reload fish config file
 abbr -a rconfig "source ~/.config/fish/config.fish"
 
-#
-# NVIM
-#
-
 abbr -a nv nvim
 
 alias mvim="NVIM_APPNAME=mvim nvim"
 
-#
-# TMUX
-#
-
-# Kill all session but current
-abbr -a tmka "tmux kill-session -a"
-
 # Load a session with tmuxp 
 abbr -a tml "tmuxp load"
 
-#
-# SSH
-#
-
-alias sshpi="TERM=xterm-256color ssh nino@nino-pi" # Connect to my pi
+# Connect to my pi
+alias sshpi="TERM=xterm-256color ssh nino@nino-pi"
