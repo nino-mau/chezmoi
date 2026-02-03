7416885ffcfe1b6f371ca92d1d0dc95816b8c802
nino.mauuary <nino.mauuary@gmail.com>
2026-02-04 00:10:43 +0100

    update random_wallpaper script

--- dot_config/exact_hypr/exact_scripts/executable_random_wallpaper.sh
+++ dot_config/exact_hypr/exact_scripts/executable_random_wallpaper.sh
@@ -6,4 +6,4 @@ WALLDIR="$HOME/.local/share/wallpapers/"
 PIC=$(find "$WALLDIR" -maxdepth 1 -type f | shuf -n 1)
 
 # set wallpaper
-swww img "$PIC" --transition-fps 60 --transition-step 255 --transition-type random
+set_wallpaper "$PIC"
