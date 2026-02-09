1604adb1f0c9de1d70900494854628e94a48539a
nino.mauuary <nino.mauuary@gmail.com>
2026-02-09 17:59:12 +0100

    update fish config

---
+++ dot_config/exact_private_fish/exact_themes/catppuccin.fish
@@ -0,0 +1,37 @@
+set -g fish_color_quote c6a0f6
+set -g fish_color_command cad3f5
+set -g fish_color_redirection b7bdf8 --bold
+set -g fish_color_end b7bdf8
+set -g fish_color_error ee99a0
+set -g fish_color_param b7bdf8
+set -g fish_color_comment b8c0e0
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match white --background=brblack
+set -g fish_color_history_current --bold
+set -g fish_color_operator b7bdf8
+set -g fish_color_escape c6a0f6
+set -g fish_color_cwd c6a0f6
+set -g fish_color_cwd_root ed8796
+set -g fish_color_option
+set -g fish_color_valid_path --underline=single
+set -g fish_color_autosuggestion 6e738d
+set -g fish_color_user a6da95
+set -g fish_color_host normal
+set -g fish_color_host_remote eed49f
+set -g fish_color_status ee99a0
+set -g fish_color_cancel ee99a0 --reverse
+
+# Pager colors
+set -g fish_pager_color_background
+set -g fish_pager_color_prefix normal --bold --underline=single
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description yellow --italics
+set -g fish_pager_color_selected_background --reverse
+set -g fish_pager_color_selected_prefix
+set -g fish_pager_color_selected_completion
+set -g fish_pager_color_selected_description
+set -g fish_pager_color_secondary_completion
+set -g fish_pager_color_secondary_description
+set -g fish_pager_color_secondary_background
+set -g fish_pager_color_secondary_prefix
