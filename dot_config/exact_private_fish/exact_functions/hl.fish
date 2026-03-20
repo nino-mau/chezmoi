22dd6d5e954ba8646d3b9c377814738b4008bea9
Nino Mauuary <nino.mauuary@gmail.com>
2026-03-20 19:46:13 +0100

    update

--- dot_config/exact_private_fish/exact_functions/hl.fish
+++ dot_config/exact_private_fish/exact_functions/hl.fish
@@ -1,6 +1,4 @@
-function hl -d 'List fish functions with descriptions'
-    set -l entries
-
+function hl -d 'Browse fish functions with fzf'
     for fn in (functions | sort)
         set -l meta (functions --details --verbose $fn)
         set -l desc $meta[5]
@@ -9,34 +7,14 @@ function hl -d 'List fish functions with descriptions'
             set desc 'no description'
         end
 
-        set -a entries (string shorten -m 44 -- "$fn - $desc")
-    end
-
-    set -l term_width $COLUMNS
-    if not string match -qr '^[0-9]+$' -- "$term_width"
-        set term_width (tput cols 2>/dev/null)
-    end
-    if not string match -qr '^[0-9]+$' -- "$term_width"
-        set term_width 120
-    end
-
-    set -l gap 4
-    set -l cell_width 44
-    set -l cols (math "max(1, floor(($term_width + $gap) / ($cell_width + $gap)))")
-    set -l rows (math "ceil("(count $entries)" / $cols)")
-    set -l spacer (string repeat -n $gap ' ')
-
-    for row in (seq 1 $rows)
-        set -l line
-
-        for col in (seq 0 (math "$cols - 1"))
-            set -l idx (math "$row + ($col * $rows)")
-
-            if test $idx -le (count $entries)
-                set -a line (string pad -w $cell_width -- $entries[$idx])
-            end
-        end
-
-        printf '%s\n' (string join -- $spacer $line)
-    end
+        printf '%s\t%s\n' $fn $desc
+    end | fzf \
+        --delimiter '\t' \
+        --with-nth=1 \
+        --preview 'printf "%s\n" {2}' \
+        --preview-window=right:65%:wrap \
+        --prompt 'functions> ' \
+        --header 'preview shows description, enter copies name, ctrl-o opens in funced' \
+        --bind 'enter:execute-silent(printf %s {1} | fish_clipboard_copy)+abort' \
+        --bind 'ctrl-o:become(fish -ic "funced {1}")'
 end
