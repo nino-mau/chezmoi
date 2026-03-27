5c65ae24fb95dcfd5ffac3d6a9e9408d07c9f1bf
Nino Mauuary <nino.mauuary@gmail.com>
2026-03-27 17:58:22 +0100

    update

--- dot_config/exact_private_fish/exact_themes/catppuccin.fish
+++ dot_config/exact_private_fish/exact_themes/catppuccin.fish
@@ -1,9 +1,9 @@
 set -g fish_color_quote cba6f7
-set -g fish_color_command cdd6f4
+set -g fish_color_command 89b4fa
 set -g fish_color_redirection 89b4fa --bold
 set -g fish_color_end 89b4fa
 set -g fish_color_error ee99a0
-set -g fish_color_param 89b4fa
+set -g fish_color_param cdd6f4
 set -g fish_color_comment bac2de
 set -g fish_color_selection cdd6f4 --bold --background=brblack
 set -g fish_color_search_match cdd6f4 --background=brblack
@@ -35,3 +35,41 @@ set -g fish_pager_color_secondary_completion
 set -g fish_pager_color_secondary_description
 set -g fish_pager_color_secondary_background
 set -g fish_pager_color_secondary_prefix
+
+# set -g fish_color_quote #cba6f7
+# set -g fish_color_command #cdd6f4
+# set -g fish_color_redirection #89b4fa --bold
+# set -g fish_color_end #89b4fa
+# set -g fish_color_error #ee99a0
+# set -g fish_color_param #89b4fa
+# set -g fish_color_comment #bac2de
+# set -g fish_color_selection #cdd6f4 --bold --background=brblack
+# set -g fish_color_search_match #cdd6f4 --background=brblack
+# set -g fish_color_history_current --bold
+# set -g fish_color_operator #89b4fa
+# set -g fish_color_escape #cba6f7
+# set -g fish_color_cwd #cba6f7
+# set -g fish_color_cwd_root #f38ba8
+# set -g fish_color_option
+# set -g fish_color_valid_path --underline=single
+# set -g fish_color_autosuggestion #6c7086
+# set -g fish_color_user #a6e3a1
+# set -g fish_color_host normal
+# set -g fish_color_host_remote #f9e2af
+# set -g fish_color_status #eba0ac
+# set -g fish_color_cancel #eba0ac --reverse
+#
+# # Pager colors
+# set -g fish_pager_color_background
+# set -g fish_pager_color_prefix normal --bold --underline=single
+# set -g fish_pager_color_progress brwhite --background=cyan
+# set -g fish_pager_color_completion normal
+# set -g fish_pager_color_description yellow --italics
+# set -g fish_pager_color_selected_background --reverse
+# set -g fish_pager_color_selected_prefix
+# set -g fish_pager_color_selected_completion
+# set -g fish_pager_color_selected_description
+# set -g fish_pager_color_secondary_completion
+# set -g fish_pager_color_secondary_description
+# set -g fish_pager_color_secondary_background
+# set -g fish_pager_color_secondary_prefix
