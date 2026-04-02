9ba6c0e076d9e97f68d50f85e1e75da613caedb2
Nino Mauuary <nino.mauuary@gmail.com>
2026-04-02 19:59:27 +0200

    update

--- dot_config/exact_private_fish/exact_functions/restart-waybar.fish
+++ dot_config/exact_private_fish/exact_functions/restart-waybar.fish
@@ -2,7 +2,7 @@ function restart-waybar --description "Restart waybar"
     pkill -9 waybar
 
     if pgrep -x Hyprland >/dev/null
-      rbg waybar
+      rbg waybar -c ~/.config/waybar/config-bar.jsonc -s ~/.config/waybar/style-bar.css
     else if pgrep -x niri >/dev/null
       rbg waybar -c ~/.config/waybar/config-bar-niri.jsonc -s ~/.config/waybar/style-bar-niri.css
     end
