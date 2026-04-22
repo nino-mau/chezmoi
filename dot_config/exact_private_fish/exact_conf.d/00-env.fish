a1bc4ed14566f2efaf5bc4b27038d129f034a03f
Nino Mauuary <nino.mauuary@smart-origin.com>
2026-04-22 09:51:22 +0200

    update

--- dot_config/exact_private_fish/exact_conf.d/00-env.fish
+++ dot_config/exact_private_fish/exact_conf.d/00-env.fish
@@ -1,13 +1,14 @@
 # Environment variables
 
+set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx EZA_CONFIG_DIR "$HOME/.config/eza"
+set -gx EXB_PATH "$HOME/Code/exb"
 set -gx PI_CODING_AGENT_DIR "$HOME/.config/pi"
+set -gx PI_CODING_AGENT_DIR "$HOME/.config/pi"
+set -x EDITOR nvim
+set -x MANPAGER "nvim +Man!"
 
 # Vi mode
 set -g fish_key_bindings fish_vi_key_bindings
 
-# Editor
-set -x EDITOR nvim
-set -x MANPAGER "nvim +Man!"
-set -gx XDG_CONFIG_HOME "$HOME/.config"
 
